54433c17400d01a2a4552425cb3893d51220f88d
angel luis sanchez bottois <angell.sanbot.2@educa.jcyl.es>
2024-04-30 20:37:59 +0200

    3d

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -31,22 +31,24 @@ export class HeaderComponent implements OnInit {
     this.renderer.setSize(200, 200);
     logo?.appendChild(this.renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(3, 3, 3);
-    const material = new THREE.MeshBasicMaterial( {color: 0X60A5FA} );
+    // const geometry = new THREE.BoxGeometry(3, 3, 3);
+    const geometry = new THREE.IcosahedronGeometry(2,1);
+    
+    const material = new THREE.MeshBasicMaterial( {color: 0X63A5FA} );
 
     const cube = new THREE.Mesh(geometry, material);
     this.logoScene.add(cube);
     this.camera.position.z = 5;
 
     const edgesGeometry = new THREE.EdgesGeometry(geometry);
-    const edgesMaterial = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 10 } );
+    const edgesMaterial = new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 2 } );
     const edges = new THREE.LineSegments(edgesGeometry, edgesMaterial);
 
     this.logoScene.add(edges);
 
     let animate = ()=>{
       requestAnimationFrame( animate );
-      cube.rotation.x += 0.006;
+      cube.rotation.x += 0.002;
       cube.rotation.y += 0.006;
       edges.rotation.copy(cube.rotation);
       this.renderer.render(this.logoScene, this.camera);
