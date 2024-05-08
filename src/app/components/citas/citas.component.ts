e87ebebc9da40df8a2bb9a5db7ebd426e2fcd242
Angel Luis Sanchez Bottois <alsanchez@clpu.es>
2024-05-08 13:21:06 +0200

    solicitud add

--- src/app/components/citas/citas.component.ts
+++ src/app/components/citas/citas.component.ts
@@ -10,7 +10,19 @@ export class CitasComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.limitarFecha();
   }
 
-  
+  limitarFecha(){
+    const fechaActual = new Date();
+    const year = fechaActual.getFullYear();
+    const month = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
+    const day = fechaActual.getDate().toString().padStart(2, '0');
+    const input: any|null = document.getElementById('fecha');
+    console.log(input);
+    
+    if(input){
+      input.min = `${year}-${month}-${day}`;
+    }
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+import { TTFLoader } from 'three/examples/jsm/loaders/TTFLoader';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+
 
 @Component({
   selector: 'app-home',
@@ -6,14 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+ 
+
 
   constructor() { }
 
   ngOnInit(): void {
-    
+    // this.createNombre();
   }
 
   info(){
     document.querySelector('.info')?.classList.add('hidden')
   }
+
+  generarNombre(){
+    const fontLoader = new FontLoader();
+    fontLoader.load(
+      'node_modules/three/examples/fonts/droid/droid_serif_regular.typeface.json',
+      (droidFont)=>{
+        const textGeometry = new TextGeometry('A3DJ',
+          {
+            height: 2,
+            size: 10,
+            font: droidFont,
+          }
+        );
+        const textMaterial = new THREE.MeshNormalMaterial();
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        textMesh.position.x = -36;
+        textMesh.position.y = 5;
+      }
+    );
+  }
+
 }
