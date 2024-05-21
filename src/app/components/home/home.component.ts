514dbaf862af9eb0210a975e997142851a817a64
angel luis sanchez bottois <angell.sanbot.2@educa.jcyl.es>
2024-05-21 10:01:42 +0200

    aviso de cookies

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,15 +10,24 @@ import { ApiService } from 'src/app/services/api/api.service';
 export class HomeComponent implements OnInit {
  
   listaPiezas: any = [];
+  allowCookies: string = "false";
 
   constructor(private cookieS: CookieService, private apiS: ApiService) { }
 
   ngOnInit(): void {
-    this.obtenerPiezasPopulares();
+    this.obtenerPiezasPopulares();  
+    if(this.cookieS.get("allowCookies")){
+      this.allowCookies = this.cookieS.get("allowCookies");
+    }  
   }
 
   info(){
     document.querySelector('.info')?.classList.add('hidden')
+  }
+
+  closeModal(){
+    this.cookieS.set("allowCookies", "true");
+    location.reload();
   }
 
   obtenerPiezasPopulares(){
